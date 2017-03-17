41a3f43bef80f98eb63da94eb7352ecb146fbe99
mvoss9000 <mvoss9000@yahoo.com>
2017-03-17 16:31:53 -0400

    format

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 // Variable Length Integer
 'use strict'
 
-
-//Functions
-//====
+// Functions
+// ====
 //
 // In Chrome (42.0.2311.152), this switch statement is roughly 4x faster than calculating: ~~((log2/Math.log(v))/7)
-function size(v) {
-    if(v < 0) {
-        throw Error('unsigned int cannot be negative: ' + v)
-    }
-    if(v < 0x80) {
-        return 1
-    } else if(v < 0x4000) {
-        return 2
-    } else if(v < 0x200000) {
-        return 3
-    } else if(v < 0x10000000) {
-        return 4
-    } else if(v < 0x800000000) {
-        return 5
-    } else if(v < 0x40000000000) {
-        return 6
-    } else if(v < 0x2000000000000) {
-        return 7
-    } else if( v > 0x1fffffffffffff) {
-        throw Error('unsigned int cannot be greater than max safe integer: ' + v)
-    } else if(isNaN(v)) {
-        throw Error('cannot convert ' + v + ' to bytes')
-    } else {
+function size (v) {
+  if (v < 0) {
+    throw Error('unsigned int cannot be negative: ' + v)
+  }
+  if (v < 0x80) {
+    return 1
+  } else if (v < 0x4000) {
+    return 2
+  } else if (v < 0x200000) {
+    return 3
+  } else if (v < 0x10000000) {
+    return 4
+  } else if (v < 0x800000000) {
+    return 5
+  } else if (v < 0x40000000000) {
+    return 6
+  } else if (v < 0x2000000000000) {
+    return 7
+  } else if (v > 0x1fffffffffffff) {
+    throw Error('unsigned int cannot be greater than max safe integer: ' + v)
+  } else if (isNaN(v)) {
+    throw Error('cannot convert ' + v + ' to bytes')
+  } else {
         /* v is between 2^49 and max safe integer */
-        return 8
-    }
+    return 8
+  }
 }
 
-// Convert an integer to bytes
-function write(v, buffer, off) {
-    if(v > 0x7FFFFFFF) {  // max positive value for signed int
-        throw Error('number too large for 32 bit conversion')   // todo: handle up to Number.MAX_SAFE_INTEGER
-    } else {
-        var n = size(v)
-        buffer[off + n - 1] = v & 0x7F          // least-significant 7 bits (no chain)
-        for(var i=n-2; i>=0; i--) {
-            v >>= 7
-            buffer[off + i] = (v & 0x7F) | 0x80 // next 7 bits with chain flag
-        }
+// write an integer to the given buffer encoded with 7-bit chaining
+function write (v, buffer, off) {
+  if (v > 0x7FFFFFFF) {  // max positive value for signed int
+    throw Error('number too large for 32 bit conversion')   // todo: handle up to Number.MAX_SAFE_INTEGER
+  } else {
+    var n = size(v)
+    buffer[off + n - 1] = v & 0x7F          // least-significant 7 bits (no chain)
+    for (var i = n - 2; i >= 0; i--) {
+      v >>= 7
+      buffer[off + i] = (v & 0x7F) | 0x80 // next 7 bits with chain flag
     }
-    return n
+  }
+  return n
 }
 
 // Read an integer value from a buffer at the given offset
@@ -54,25 +53,25 @@ function write(v, buffer, off) {
 // If h is given (a holder array), then write the integer value and the
 // new buffer offset into the array at h[0] and h[1].
 // If h is not given, just return the integer value.
-function read(buf, off, h) {
-    var noff = off
-    var p = buf[noff++]
-    var v = p & 0x7F        // put low-order 7 bits
-    while(p & 0x80) {       // check hi bit
-        v <<= 7
-        p = buf[noff++]
-        v |= (p & 0x7F)     // put low-order 7 bits
-    }
-    if(h) {
-        h[0] = v
-        h[1] = noff
-    } else {
-        return v
-    }
+function read (buf, off, h) {
+  var noff = off
+  var p = buf[noff++]
+  var v = p & 0x7F        // put low-order 7 bits
+  while (p & 0x80) {       // check hi bit
+    v <<= 7
+    p = buf[noff++]
+    v |= (p & 0x7F)     // put low-order 7 bits
+  }
+  if (h) {
+    h[0] = v
+    h[1] = noff
+  } else {
+    return v
+  }
 }
 
 module.exports = {
-    write: write,
-    read: read,
-    size: size
+  write: write,
+  read: read,
+  size: size
 }
--- test.js
+++ test.js
@@ -2,112 +2,112 @@ var test = require('test-kit').tape()
 
 var uint = require('.')
 
-test('uint: throws', function(t) {
-    var buf = new Uint8Array(5)
-    t.tableAssert(
-        [
-            [ 'fn', 'input',  'exp' ],
-            [ 'size', [Math.pow(2, 53)],    /greater than max safe integer/ ],
-            [ 'size', [1/0],                /greater than max safe integer/ ],
-            [ 'size', [-1],                 /cannot be negative/ ],
-            [ 'size', [-1/0],               /cannot be negative/ ],
-            [ 'size', [],                   /cannot convert/    ],
-            [ 'write', [0x7FFFFFFF+1, buf], /too large/         ],
-        ],
-        function(fn, input) {
-            uint[fn].apply(null, input)
-        },
-        { assert: 'throws' }
+test('uint: throws', function (t) {
+  var buf = new Uint8Array(5)
+  t.tableAssert(
+    [
+      [ 'fn',      'input',                'exp' ],
+      [ 'size',    [Math.pow(2, 53)],      /greater than max safe integer/ ],
+      [ 'size',    [1 / 0],                /greater than max safe integer/ ],
+      [ 'size',    [-1],                   /cannot be negative/ ],
+      [ 'size',    [-1 / 0],               /cannot be negative/ ],
+      [ 'size',    [],                     /cannot convert/ ],
+      [ 'write',   [0x7FFFFFFF + 1, buf],  /too large/ ],
+    ],
+      function (fn, input) {
+        uint[fn].apply(null, input)
+      },
+      { assert: 'throws' }
     )
 })
 
-test('uint: size', function(t) {
-    t.equal(uint.size(0), 1)
-    for (var i = 1; i <= 7; i++) {
-        t.equal(uint.size(Math.pow(2, i * 7) - 1), i)
-        t.equal(uint.size(Math.pow(2, i * 7)), i + 1)
+test('uint: size', function (t) {
+  t.equal(uint.size(0), 1)
+  for (var i = 1; i <= 7; i++) {
+    t.equal(uint.size(Math.pow(2, i * 7) - 1), i)
+    t.equal(uint.size(Math.pow(2, i * 7)), i + 1)
         // console.log('# ' + (Math.pow(2, i * 7) - 1))
-    }
-    var maxInt = Math.pow(2, 53) - 1
-    t.equal(uint.size(maxInt), 8)
-    t.end()
+  }
+  var maxInt = Math.pow(2, 53) - 1
+  t.equal(uint.size(maxInt), 8)
+  t.end()
 })
 
-test('uint: read with offset',  function(t) {
-    t.tableAssert([
-        // input buffer           // value/offset expected
-        [ 'a',                    'exp'                ],
-        [ [0,0,0,0,0],            [ 0, 1          ]    ],
-        [ [1,0,0,0,0],            [ 1, 1          ]    ],
-        [ [127,0,0,0,0],          [ 127, 1        ]    ],
-        [ [129,0,0,0,0],          [ 128, 2        ]    ],
-        [ [255,127,0,0,0],        [ 16383, 2      ]    ],
-        [ [129,128,0,0,0],        [ 16384, 3      ]    ],
-        [ [255,255,127,0,0],      [ 2097151, 3    ]    ],
-        [ [129,128,128,0,0],      [ 2097152, 4    ]    ],
-        [ [255,255,255,127,0],    [ 268435455, 4  ]    ],
-        [ [129,128,128,128,0],    [ 268435456, 5  ]    ],
-        [ [135,255,255,255,127],  [ 2147483647, 5 ]    ],
-    ], function(a){
-        var buf = new Uint8Array(a)
-        var value_and_offset = []
-        uint.read(buf, 0, value_and_offset)
-        return value_and_offset
-    })
+test('uint: read with offset', function (t) {
+  t.tableAssert([
+    // input buffer               // value/offset expected
+    [ 'a',                        'exp' ],
+    [ [0, 0, 0, 0, 0],            [ 0, 1 ] ],
+    [ [1, 0, 0, 0, 0],            [ 1, 1 ] ],
+    [ [127, 0, 0, 0, 0],          [ 127, 1 ] ],
+    [ [129, 0, 0, 0, 0],          [ 128, 2 ] ],
+    [ [255, 127, 0, 0, 0],        [ 16383, 2 ] ],
+    [ [129, 128, 0, 0, 0],        [ 16384, 3 ] ],
+    [ [255, 255, 127, 0, 0],      [ 2097151, 3 ] ],
+    [ [129, 128, 128, 0, 0],      [ 2097152, 4 ] ],
+    [ [255, 255, 255, 127, 0],    [ 268435455, 4 ] ],
+    [ [129, 128, 128, 128, 0],    [ 268435456, 5 ] ],
+    [ [135, 255, 255, 255, 127],  [ 2147483647, 5 ] ]
+  ], function (a) {
+    var buf = new Uint8Array(a)
+    var value_and_offset = []
+    uint.read(buf, 0, value_and_offset)
+    return value_and_offset
+  })
 })
 
-test('uint: read',  function(t) {
-    t.tableAssert([
-        // input buffer           // value/offset expected
-        [ 'a',                    'exp'                ],
-        [ [0,0,0,0,0],            0          ],
-        [ [1,0,0,0,0],            1          ],
-        [ [127,0,0,0,0],          127        ],
-        [ [129,0,0,0,0],          128        ],
-        [ [255,127,0,0,0],        16383      ],
-        [ [129,128,0,0,0],        16384      ],
-        [ [255,255,127,0,0],      2097151     ],
-        [ [129,128,128,0,0],      2097152     ],
-        [ [255,255,255,127,0],    268435455    ],
-        [ [129,128,128,128,0],    268435456    ],
-        [ [135,255,255,255,127],  2147483647    ],
-    ], function(a){
-        return uint.read(new Uint8Array(a), 0)
-    })
+test('uint: read', function (t) {
+  t.tableAssert([
+    // input buffer           // value/offset expected
+    [ 'a', 'exp' ],
+    [ [0, 0, 0, 0, 0], 0 ],
+    [ [1, 0, 0, 0, 0], 1 ],
+    [ [127, 0, 0, 0, 0], 127 ],
+    [ [129, 0, 0, 0, 0], 128 ],
+    [ [255, 127, 0, 0, 0], 16383 ],
+    [ [129, 128, 0, 0, 0], 16384 ],
+    [ [255, 255, 127, 0, 0], 2097151 ],
+    [ [129, 128, 128, 0, 0], 2097152 ],
+    [ [255, 255, 255, 127, 0], 268435455 ],
+    [ [129, 128, 128, 128, 0], 268435456 ],
+    [ [135, 255, 255, 255, 127], 2147483647 ],
+  ], function (a) {
+    return uint.read(new Uint8Array(a), 0)
+  })
 })
 
-test('uint: write',  function(t) {
-    t.tableAssert([
-        ['v',          'exp'                 ],
-        [ 0,           [0,0,0,0,0]           ],
-        [ 1,           [1,0,0,0,0]           ],
-        [ 127,         [127,0,0,0,0]         ],
-        [ 128,         [129,0,0,0,0]         ],
-        [ 16383,       [255,127,0,0,0]       ],
-        [ 16384,       [129,128,0,0,0]       ],
-        [ 2097151,     [255,255,127,0,0]     ],
-        [ 2097152,     [129,128,128,0,0]     ],
-        [ 268435455,   [255,255,255,127,0]   ],
-        [ 268435456,   [129,128,128,128,0]   ],
-        [ 2147483647,  [135,255,255,255,127] ],
-    ], function(v){
-        var buf = new Uint8Array(5)
-        uint.write(v, buf, 0)
-        return Array.prototype.slice.call(buf)
-    })
+test('uint: write', function (t) {
+  t.tableAssert([
+    ['v',             'exp' ],
+    [ 0,              [0, 0, 0, 0, 0] ],
+    [ 1,              [1, 0, 0, 0, 0] ],
+    [ 127,            [127, 0, 0, 0, 0] ],
+    [ 128,            [129, 0, 0, 0, 0] ],
+    [ 16383,          [255, 127, 0, 0, 0] ],
+    [ 16384,          [129, 128, 0, 0, 0] ],
+    [ 2097151,        [255, 255, 127, 0, 0] ],
+    [ 2097152,        [129, 128, 128, 0, 0] ],
+    [ 268435455,      [255, 255, 255, 127, 0] ],
+    [ 268435456,      [129, 128, 128, 128, 0] ],
+    [ 2147483647,     [135, 255, 255, 255, 127] ],
+  ], function (v) {
+    var buf = new Uint8Array(5)
+    uint.write(v, buf, 0)
+    return Array.prototype.slice.call(buf)
+  })
 })
 
-test('uint: write with offset',  function(t) {
-    t.tableAssert([
-        [ 'v',    'off', 'exp'     ],
-        [ 16383,  0,     [255,127,0,0,0,0] ],
-        [ 16383,  1,     [0,255,127,0,0,0] ],
-        [ 16383,  2,     [0,0,255,127,0,0] ],
-        [ 16383,  3,     [0,0,0,255,127,0] ],
-        [ 16383,  4,     [0,0,0,0,255,127] ],
-    ], function(v, off) {
-        var buf = new Uint8Array(6)
-        uint.write(v, buf, off)
-        return Array.prototype.slice.call(buf)
-    })
+test('uint: write with offset', function (t) {
+  t.tableAssert([
+        [ 'v',     'off',  'exp' ],
+        [ 16383,   0,      [255, 127, 0, 0, 0, 0] ],
+        [ 16383,   1,      [0, 255, 127, 0, 0, 0] ],
+        [ 16383,   2,      [0, 0, 255, 127, 0, 0] ],
+        [ 16383,   3,      [0, 0, 0, 255, 127, 0] ],
+        [ 16383,   4,      [0, 0, 0, 0, 255, 127] ],
+  ], function (v, off) {
+    var buf = new Uint8Array(6)
+    uint.write(v, buf, off)
+    return Array.prototype.slice.call(buf)
+  })
 })
